2369de89c2fbcf046d7571d8843dd57956aa48ba
Rhys Davis <rhysdavis1927@gmail.com>
2014-12-31 15:52:00 +0000

    first commit

---
+++ src/main/java/com/conversant/myproject/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.conversant.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/")
+    public String index(Model model) {
+        model.addAttribute("issues", "something");
+        return "index";
+    }
+}
---
+++ src/test/java/com/conversant/myproject/AppTest.java
@@ -0,0 +1,14 @@
+package com.conversant.myproject;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void appendStringAppendsStringToEachMemberOfCollection() {
+		//		fail("Not yet implemented");
+	}
+
+}
